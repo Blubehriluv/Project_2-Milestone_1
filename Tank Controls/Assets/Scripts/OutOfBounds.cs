0c57b7048f4b6a929f1ac40e2fbb08bed5c8989b
Taylor McKenzie <44045526+Blubehriluv@users.noreply.github.com>
2019-02-02 03:21:26 -0700

    Added Sprites, Added Collider, Death Text
    Only placed the sprites into their correct folder so far. Collider script isn't working yet. Death Text will work properly once the collider script does.

---
+++ Tank Controls/Assets/Scripts/OutOfBounds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("I died bro");
+            Destroy(gameObject);
+        }
+
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+        if (collision.gameObject.tag == "Space Rock")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Space Rock ded");
+        }
+    }
+}
