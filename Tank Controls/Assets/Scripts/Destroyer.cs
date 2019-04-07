b14d0bb70bd1d172c31e0123c391d08a9dd40fdb
Taylor McKenzie <taymcken@uat.edu>
2019-04-07 14:38:21 -0700

    Recreated Player Prefab with Play Area
    When the player leaves the play area, it is destroyed.

--- Tank Controls/Assets/Scripts/Destroyer.cs
+++ Tank Controls/Assets/Scripts/Destroyer.cs
@@ -1,16 +1,11 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class Destroyer : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class Destroyer : MonoBehaviour
+{
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+    }
 }
