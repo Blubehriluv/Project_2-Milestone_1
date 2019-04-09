ce360a288f1a73c08071516abfb05c16b3216a0b
Taylor McKenzie <xx20twinkiie14xx@gmail.com>
2019-04-08 17:16:31 -0700

    Changed Code for Enemy Spawn
    Still a fucking failure

--- Tank Controls/Assets/Scripts/EnemyManager.cs
+++ Tank Controls/Assets/Scripts/EnemyManager.cs
@@ -7,13 +7,17 @@ public class EnemyManager : MonoBehaviour
     //public PlayerHealth playerHealth;       // Reference to the player's heatlh.
     public GameObject Enemy;                // The enemy prefab to be spawned.
     public float spawnTime = 3f;            // How long between each spawn.
-    public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+    //public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+    //public Vector3 spawnPoint;
+    private int spawnPointX;
+    private int spawnPointY;
 
 
     void Start()
     {
         // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
         InvokeRepeating("Spawn", spawnTime, spawnTime);
+        
     }
 
 
@@ -27,9 +31,39 @@ public class EnemyManager : MonoBehaviour
         }*/
 
         // Find a random index between zero and one less than the number of spawn points.
-        //int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        int whichPoint = Random.Range(0, 3);
+        
+
+        if (whichPoint == 0)
+        {
+            spawnPointX = 0;
+            spawnPointY = -12;
+        }
+
+        if (whichPoint == 1)
+        {
+            //spawnPoint = new Vector3(0, 12, 0);
+            spawnPointX = 0;
+            spawnPointY = 12;
+        }
+
+        if (whichPoint == 2)
+        {
+            //spawnPoint = new Vector3(-12, 0, 0);
+            spawnPointX = -12;
+            spawnPointY = 0;
+        }
+
+        if (whichPoint == 3)
+        {
+            //spawnPoint = new Vector3(12, 0, 0);
+            spawnPointX = 12;
+            spawnPointY = 0;
+        }
+        Vector3 spawnPoint = new Vector3(spawnPointX, spawnPointY, 0);
+
 
         // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-        Instantiate(Enemy/*, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation*/);
+        Instantiate(Enemy, spawnPoint, Quaternion.identity);
     }
 }
